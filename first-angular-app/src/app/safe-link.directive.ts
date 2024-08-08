68f55fc82a2e071f4b28f9f6a92a26eaf47673a8
MahmoudAhmed <mahmoud.elgawad@gmail.com>
2024-08-08 13:31:09 +0300

    -angualr: custom attribute directrive

--- first-angular-app/src/app/safe-link.directive.ts
+++ first-angular-app/src/app/safe-link.directive.ts
@@ -1,4 +1,4 @@
-import { Directive } from "@angular/core";
+import { Directive, ElementRef, inject, input } from "@angular/core";
 
 @Directive({
     selector:'a[appSafeLink]',
@@ -7,7 +7,11 @@ import { Directive } from "@angular/core";
         '(click)' :'onConfirmLeavePage($event)'
     }
 })
-export class SafeLinkDirective{
+export class SafeLinkDirective{ // custome attribute  directive
+    
+    //input take same aliase name of current directive
+    queryParam = input("ebcell.com",{alias:'appSafeLink'});
+    hostAnchorElementRef = inject<ElementRef<HTMLAnchorElement>>(ElementRef);
     constructor(){
         console.log("from SafeLinkDirective");
     }
@@ -15,6 +19,14 @@ export class SafeLinkDirective{
     onConfirmLeavePage(event:MouseEvent){
         const wantLeave = window.confirm("Do you want to Leave ?");
         if(wantLeave){
+            //before leave , edit current host anchor href address with my website programmatically
+            this.hostAnchorElementRef.nativeElement.href = 
+                this.hostAnchorElementRef.nativeElement.href + "?from="+ this.queryParam();
+
+            //old way
+            /*let anchor = (event.target as HTMLAnchorElement);
+            anchor.href = anchor.href + "?from="+ this.queryParam();
+            */
             return;
         }
         event.preventDefault();
