e21f94f8d60dd8341ae51423803fd2d1368ff679
MahmoudAhmed <mahmoud.elgawad@gmail.com>
2024-08-12 16:03:07 +0300

    Angualr - host directive

--- first-angular-app/src/app/app.component.ts
+++ first-angular-app/src/app/app.component.ts
@@ -5,6 +5,7 @@ import { AuthComponent } from './auth/auth.component';
 import { LearningResourcesComponent } from './learning-resources/learning-resources.component';
 import { AuthService } from './auth/auth.service';
 import { AuthDirective } from './auth/auth.directive';
+import { LogDirective } from './log.directive';
 
 
 @Component({
@@ -12,7 +13,10 @@ import { AuthDirective } from './auth/auth.directive';
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [AuthComponent, LearningResourcesComponent, NgIf, AuthDirective],
+  imports: [AuthComponent, LearningResourcesComponent, NgIf, AuthDirective, LogDirective],
+  //use host directive on app component, mean add directive on current host component 'app'
+  //and will log any click with that component
+  // hostDirectives:[LogDirective]
 })
 export class AppComponent {
 private authService = inject(AuthService)
