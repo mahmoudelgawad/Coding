8c0565ba0c39a268e042b61192de1f53e9cdef07
MahmoudAhmed <mahmoud.elgawad@gmail.com>
2024-08-30 12:41:52 +0300

    angualr - OnPush change detection

--- first-angular-app/src/app/messages/messages.component.ts
+++ first-angular-app/src/app/messages/messages.component.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 
 import { MessagesListComponent } from './messages-list/messages-list.component';
 import { NewMessageComponent } from './new-message/new-message.component';
@@ -9,6 +9,12 @@ import { NewMessageComponent } from './new-message/new-message.component';
   templateUrl: './messages.component.html',
   styleUrl: './messages.component.css',
   imports: [MessagesListComponent, NewMessageComponent],
+  // it's less aware of events listeners outside compoment
+  // or less evaluate changes on current compoennt, with unnessary detect changes
+  //but not restrict events send changes to other components
+  // enhance performance effecincy , if your project big
+  // and your component have no inputs
+  changeDetection:ChangeDetectionStrategy.OnPush
 })
 export class MessagesComponent {
   messages = signal<string[]>([]);
