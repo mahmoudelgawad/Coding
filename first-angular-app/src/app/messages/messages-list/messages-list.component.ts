f6ef0ec4d3474891cfe300a97a4b517f7d6079ee
MahmoudAhmed <mahmoud.elgawad@gmail.com>
2024-09-02 13:32:09 +0300

    Angualr - use rxjs observable instead signal objects for detect changes

--- first-angular-app/src/app/messages/messages-list/messages-list.component.ts
+++ first-angular-app/src/app/messages/messages-list/messages-list.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, inject, input } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, DestroyRef, inject, input, OnInit } from '@angular/core';
 import { MessagesService } from '../messages.service';
 
 @Component({
@@ -8,11 +8,29 @@ import { MessagesService } from '../messages.service';
   styleUrl: './messages-list.component.css',
   changeDetection:ChangeDetectionStrategy.OnPush
 })
-export class MessagesListComponent {
+export class MessagesListComponent implements OnInit {
   // messages = input.required<string[]>();
-  // private to not allow access service instance from the view template
- private messagesService = inject(MessagesService);
- messages = this.messagesService.allMessages;
+
+// private to not allow access service instance from the view template
+private messagesService = inject(MessagesService);
+//  messages = this.messagesService.allMessages;
+
+ //to check changes manually, when not use signal object
+ cdr = inject(ChangeDetectorRef);
+ destroyRef = inject(DestroyRef);
+ messages: string[] = [];
+ ngOnInit(): void {
+   let messagesSubscribtion = this.messagesService.messages$.subscribe((result) =>{
+    this.messages = result;
+    //if not exist will not detect changes
+    //this.cdr.markForCheck();
+   });
+
+   //for best breformance for components
+   this.destroyRef.onDestroy(() =>{
+    messagesSubscribtion.unsubscribe();
+   });
+ }
 
   get debugOutput() {
     console.log('[MessagesList] "debugOutput" binding re-evaluated.');
--- first-angular-app/src/app/messages/messages.service.ts
+++ first-angular-app/src/app/messages/messages.service.ts
@@ -1,14 +1,31 @@
 import { Injectable, signal } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MessagesService {
-private messages = signal<string[]>([]);
-allMessages = this.messages.asReadonly();
+//using signal object
+// private messages = signal<string[]>([]);
+// allMessages = this.messages.asReadonly();
+
+//using rxjs observable object
+private messages : string[]=[];
+messages$ = new BehaviorSubject<string[]>([]);
+
+//using signal
+  // addMessage(message:string){
+  //   this.messages.update((old) =>{
+  //     return [...old,message]
+  //   });
+  // }
+
+
+  //using rxjs observable
   addMessage(message:string){
-    this.messages.update((old) =>{
-      return [...old,message]
-    });
+    this.messages = [...this.messages,message];
+    this.messages$.next(this.messages);
   }
+
+
 }
