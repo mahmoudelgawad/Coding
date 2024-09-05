25e3d67dc0dc0f0f4e33904532a0001b86f73e48
MahmoudAhmed <mahmoud.elgawad@gmail.com>
2024-09-06 00:51:03 +0300

    Angualr - rxjs interval

--- first-angular-app/src/app/app.component.ts
+++ first-angular-app/src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+value:number = 0
+  ngOnInit(){
+    
+    interval(1000).subscribe({
+      next:(value) => console.log(value)
+    });
 
+    // setInterval(() => {
+    //   console.log(this.value++);
+    // }, 1000);
+  }
 }
