73572bbb49319ac27ef8d4fcafb8891d1c1c06c3
MahmoudAhmed <mahmoud.elgawad@gmail.com>
2024-09-06 00:20:58 +0300

    Angualr - start sec 11

--- first-angular-app/src/main.ts
+++ first-angular-app/src/main.ts
@@ -1,9 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-//mean Zone.js ignored for detect changes
-//specially when have setTimeOut Timer on change normal variable
-//so use signal object instead to check changes during Zone.js ignored
-//import { provideExperimentalZonelessChangeDetection } from '@angular/core';
+
 import { AppComponent } from './app/app.component';
 
-
-bootstrapApplication(AppComponent).catch((err) => console.error(err));
+bootstrapApplication(AppComponent)
+  .catch((err) => console.error(err));
