b8b209166221fbd0c9f47644fffd9161da6ab7d4
MahmoudAhmed <mahmoud.elgawad@gmail.com>
2024-10-09 13:51:48 +0300

    Angualr - Interceptors config in main angular file, changes for cloned request and request.headers

--- Angualr_PlayGround/first-angular-app/src/main.ts
+++ Angualr_PlayGround/first-angular-app/src/main.ts
@@ -1,9 +1,17 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 
 import { AppComponent } from './app/app.component';
-import { provideHttpClient } from '@angular/common/http';
+import { HttpHandlerFn, HttpRequest, provideHttpClient, withInterceptors } from '@angular/common/http';
 
+function LoggingInterCeptor(request:HttpRequest<unknown>, next:HttpHandlerFn){
+    console.log('LoggingInterCeptor:OutgoingRequest');
+    console.log(request);
+    const reqClone = request.clone({
+        headers: request.headers.set("F-DEBUG",'valueTestFofo')
+    });
+ return next(reqClone);
+}
 
 bootstrapApplication(AppComponent,{
-    providers:[provideHttpClient()]
+    providers:[provideHttpClient(withInterceptors([LoggingInterCeptor]))]
 }).catch((err) => console.error(err));
