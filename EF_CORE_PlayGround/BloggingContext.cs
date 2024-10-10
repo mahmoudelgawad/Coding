0a35c4c23ab864ba5561c3bfedddc4ef1a05b3f8
MahmoudAhmed <mahmoud.elgawad@gmail.com>
2024-10-10 21:09:50 +0300

    C# - EF core project

---
+++ EF_CORE_PlayGround/BloggingContext.cs
@@ -0,0 +1,16 @@
+﻿using EF_CORE_PlayGround.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EF_CORE_PlayGround
+{
+    public class BloggingContext :DbContext
+    {
+        DbSet<Blog> Blogs { get; set; }
+        DbSet<Post> Posts { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=DESKTOP-M947CDC;Database=DB_PlayGround;Integrated Security=True;Connect Timeout=30;Encrypt=True;Trust Server Certificate=True;Application Intent=ReadWrite;Multi Subnet Failover=False");
+        }
+    }
+}
---
+++ EF_CORE_PlayGround/Models/Post.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF_CORE_PlayGround.Models
+{
+    public class Post
+    {
+        public int PostId { get; set; }
+        public int BlogId { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        
+        public Blog Blog { get; set; }
+
+    }
+}
