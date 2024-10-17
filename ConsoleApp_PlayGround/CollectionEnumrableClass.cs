bc07deb0e5669083263ef8fa62382a1f34daa70c
MahmoudAhmed <mahmoud.elgawad@gmail.com>
2024-10-17 19:22:37 +0300

    c#- HashSet , Dictionary

--- ConsoleApp_PlayGround/CollectionEnumrableClass.cs
+++ ConsoleApp_PlayGround/CollectionEnumrableClass.cs
@@ -13,6 +13,15 @@ namespace ConsoleApp_PlayGround
             var list = new List<int> { 1, 5, 6, 10, 9, 16, 20 };
             //the query itself not implemented untill call Tolist or query with "Linq"
             list.Where(n => n > 10).ToList();
+
+            /* HashSet<int> set = new HashSet<int>();
+             set[0].
+            */
+
+            Dictionary<string, int> dict = new Dictionary<string,int>();
+            var intValueDict = dict["key"]; // index as string same 'Key' type
+
+            var intValueList = list[0];
         }
     }
 }
