a85c41c176349a97816e79ee98bb6dba7dce3446
MahmoudAhmed <mahmoud.elgawad@gmail.com>
2024-10-22 15:06:04 +0300

    angualr - template driven form

--- Angualr_PlayGround/first-angular-app/src/app/auth/login/login.component.ts
+++ Angualr_PlayGround/first-angular-app/src/app/auth/login/login.component.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
+  imports:[FormsModule]
 })
-export class LoginComponent {}
+export class LoginComponent {
+
+  onSubmit(form:NgForm){
+    console.log(form);
+  }
+}
