7b546897286232343ae83ec87852ac6ec794a4a3
mahmoudelgawad <mahmoud.elgawad@gmail.com>
2024-11-13 11:00:09 +0200

    angualr - FormArray

---
+++ ConsoleApp_PlayGround/ExceptionHandling.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_PlayGround
+{
+    internal class ExceptionHandling
+    {
+        public void MainFun() 
+        {
+            Fun1();
+        }
+        public void Fun1()
+        {
+            try
+            {
+                Fun2();
+            }
+            catch(Exception)
+            {
+                throw;
+            }
+            finally 
+            {
+                Console.WriteLine("finally of Fun1 process");
+            }
+        }
+
+        public void Fun2() 
+        {            
+           throw new Exception("Exception from Fun2");         
+        }
+    }
+
+   public class ExceptionHandlingImplementation
+    {
+        public static void Implement() 
+        {            
+            var yieldRetrunObj = new YieldReturnClass();
+            var values = yieldRetrunObj.getStringValues();
+            Console.WriteLine(string.Join(",",values.ToArray()));
+
+            foreach (var i in yieldRetrunObj.getTotal()) 
+            {
+                Console.WriteLine(i);
+            }
+        }
+     
+    }
+}
