dac426244ba7cf3d1cf4943f676f55c131c63435
mahmoudelgawad <mahmoud.elgawad@gmail.com>
2024-11-14 10:59:04 +0200

    c# comment on attribute

--- ConsoleApp_PlayGround/AttributeInfo/HelpTextAttribute.cs
+++ ConsoleApp_PlayGround/AttributeInfo/HelpTextAttribute.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp_PlayGround.AttributeInfo
 {
+    //public abstract partial class Attribute,
+    //contains private and public methods
     internal class HelpTextAttribute : Attribute
     {
         public string HelpText { get; set; }
