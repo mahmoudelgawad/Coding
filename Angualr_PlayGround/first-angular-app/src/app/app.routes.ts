eb8b2108a894cd48eba582fde4850dfa57eed00c
mahmoudelgawad <mahmoud.elgawad@gmail.com>
2024-12-13 20:18:14 +0200

    angualr-child router

--- Angualr_PlayGround/first-angular-app/src/app/app.routes.ts
+++ Angualr_PlayGround/first-angular-app/src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from "@angular/router";
 import { TasksComponent } from "./tasks/tasks.component";
 import { NoTaskComponent } from "./tasks/no-task/no-task.component";
 import { UserTasksComponent } from "./users/user-tasks/user-tasks.component";
+import { NewTaskComponent } from "./tasks/new-task/new-task.component";
 
 export const appRoutes :Routes = [
     {
@@ -25,6 +26,10 @@ export const appRoutes :Routes = [
         {
             path:'tasks', // yourdomain.com/users/<userId>/tasks
             component: TasksComponent,
+        },
+        {
+            path:"tasks/new",
+            component:NewTaskComponent
         }
        ]
     }];
