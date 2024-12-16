ff6236a121d31ba0a874a8540000546f5f32ec76
mahmoudelgawad <mahmoud.elgawad@gmail.com>
2024-12-16 22:38:01 +0200

    angualr - route config child components input binding url param

--- Angualr_PlayGround/first-angular-app/src/app/app.config.ts
+++ Angualr_PlayGround/first-angular-app/src/app/app.config.ts
@@ -1,7 +1,13 @@
 import { ApplicationConfig } from "@angular/core";
-import { provideRouter, withComponentInputBinding } from "@angular/router";
+import { provideRouter, withComponentInputBinding, withRouterConfig } from "@angular/router";
 import { appRoutes } from "./app.routes";
 
 export const appConfig : ApplicationConfig = {
-    providers:[provideRouter(appRoutes,withComponentInputBinding())]
+    providers:[
+        provideRouter(
+            appRoutes,
+            //applicable for main/parent component/route, not child/nested routes
+            withComponentInputBinding(),
+            withRouterConfig({paramsInheritanceStrategy:"always"})),
+    ]
 }
