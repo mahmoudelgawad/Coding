43e3c5a841229ffc61d0b70ceec57c9c76a30969
mahmoudelgawad <mahmoud.elgawad@gmail.com>
2025-02-04 22:46:55 +0200

    c# - implement disposable

---
+++ ConsoleApp_PlayGround/Disposable/UnmanagementClass.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_PlayGround
+{
+    internal class UnmanagementClass:IDisposable
+    {
+        ~UnmanagementClass()
+        {
+            //incase forgot to call Dispose()
+            Dispose(false);
+        }
+
+        private void Dispose(bool isDisposingManagemendCode) 
+        {
+            if (isDisposingManagemendCode) 
+            {
+             // clean management code objects
+            }
+
+            // clean Unmanagement code objects
+        }
+
+        public void Dispose() 
+        {
+            //addition free management code logic
+            Dispose(true);
+
+            //ensure the destructor not cllaing and clean the object from first cycle
+            GC.SuppressFinalize(this);
+        }
+
+    }
+}
