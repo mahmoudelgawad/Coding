c371b9a59add01234e59f22f6645ea621f6d4c67
mahmoudelgawad <mahmoud.elgawad@gmail.com>
2025-02-11 09:53:01 +0200

    push changes

--- Angular_PlayGround/first-angular-app/src/app/app.component.ts
+++ Angular_PlayGround/first-angular-app/src/app/app.component.ts
@@ -12,3 +12,28 @@ import { UsersComponent } from './users/users.component';
   imports: [HeaderComponent, UsersComponent, RouterOutlet],
 })
 export class AppComponent {}
+
+/*
+-ng build
+-npm run build //(will run 'ng build' under the hood)
+
+CONS
+  1-to optimize the code - bundle szie
+  2-genrate output path 'dist\routing\browser'
+
+#hosting on firebase "firbase.google.com"
+-hosting
+-DB
+
+-npm intsall -g firebase-tools //on global system device
+-init firebase //on local project path
+>hosting:Configuration file for firebase hosting (select)
+
+-firebase deploy //will push on firebase your output then genrate dummy URL domain createds
+
+#MANUALL WAY FOR PREVIOUS STEPS#
+
+-ng add @angular/fire
+-ng deploy
+  
+*/
