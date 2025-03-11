d30c94e1a81a48b19f492ad111af54a7edca7257
mahmoudelgawad <mahmoud.elgawad@gmail.com>
2025-03-11 15:10:57 +0200

    SQL server- tran rollback block

--- DB_PlayGround/Challenges/Highest 2 Employee Salary.sql
+++ DB_PlayGround/Challenges/Highest 2 Employee Salary.sql
@@ -1,15 +1,20 @@
+-- optional final salaries order by defualt ASC
+--must internal top 3 (*) salries order by DESC
 /*
 declare @nHight int = 3;
 
 select top 1 id,FirstName,MiddleName,salary
-from (select top 2 * from employee order by salary DESC) empTop
+from (select top 3 * from employee order by salary DESC) empTop
 order by Salary
 */
 
 -- co related
+-- salaries ordered by default ASC 
+--/*
 declare @nth int = 3;
 select e1.salary from employee e1
 where @nth = ( 
 select count(*) from Employee as e2
 where e1.Salary > e2.Salary
 )
+--*/
