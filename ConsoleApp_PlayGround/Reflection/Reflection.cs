eb3a24a383e91c57af106f0771d19d3ae7629ce2
mahmoudelgawad <mahmoud.elgawad@gmail.com>
2025-05-07 00:41:53 +0300

    c#

---
+++ ConsoleApp_PlayGround/Reflection/Reflection.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace ConsoleApp_PlayGround
+{
+    /*
+     "MyLibrary1" -> "Class1"
+    1- load assembly (path of dll)
+    2- Get Type of loaded assembly (namespace.className )
+    3- Activate classNameType
+    4-Get Type of className
+
+        -----------------
+    classNameType.GetMembers() //return 'MemberInfo'
+    classNameType.GetProperties() //return 'PropertyInfo'
+        -----------------
+    classNameType.InvokeMember("name", options...);
+        -----------------
+     */
+
+    internal class Reflection
+    {
+        void test() 
+        {
+            Assembly.LoadFile("path of dll");
+        }
+    }
+}
