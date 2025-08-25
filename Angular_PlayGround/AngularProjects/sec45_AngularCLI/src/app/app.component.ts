678cb70a2ef7e067fc0a456f7b05d786cf665886
mahmoudelgawad <mahmoud.elgawad@gmail.com>
2025-08-25 08:46:06 +0300

    Angular - CLI and configuration

--- Angular_PlayGround/AngularProjects/sec45_AngularCLI/src/app/app.component.ts
+++ Angular_PlayGround/AngularProjects/sec45_AngularCLI/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MainNavComponent } from "./main-nav/main-nav.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MainNavComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
