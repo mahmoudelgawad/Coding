0712c9e89f1dad9a1f754df4bf2882b051246f01
mahmoudelgawad <mahmoud.elgawad@gmail.com>
2026-02-02 10:12:46 +0200

    changes no 2 for 2 Feb 20226

--- React_PlayGround/ReactProjects/03_ReactEssentials/src/App.jsx
+++ React_PlayGround/ReactProjects/03_ReactEssentials/src/App.jsx
@@ -1,38 +1,12 @@
 import {useState} from 'react'
 import Header from './components/Header/Header.jsx';
 import CoreConcept from './components/CoreConcept.jsx';
-import TabButton from './components/TabButton.jsx';
+import imgConc1 from './assets/react-core-concepts.png';
+import Examples from './components/Examples.jsx'
+import {CORE_CONCEPTS} from './data.js';
 
-import imgConc1 from './assets/react-core-concepts.png'
-import {CORE_CONCEPTS, EXAMPLES} from './data.js';
 
-
-function App() {
-  //#must use 'useState' with component function and in the top of it,it alwyas return array with2 values
-  //const [stringValue,onSetStateActionNewStringValue] = useState('Initial Value - Please Select Press button');
-  const [stringValue,onSetStateActionNewStringValue] = useState();
-  //#deprectaed
-  //let stringValue = 'Please Select Press button';
-      function onHandleSelect(selectedValue){
-        onSetStateActionNewStringValue(selectedValue);
-        //#deprecated
-        //stringValue = selectedValue;
-        console.log(`You Select -> ${selectedValue}`);
-    }
-
-    let tabContent = <p>Please select content Example ...</p>
-    if(stringValue)
-      tabContent = (
-        <div id="tab-content">
-            <h3>{EXAMPLES[stringValue]?.title}</h3>
-            <p>{EXAMPLES[stringValue]?.description}</p>
-            <pre>
-              <code>{EXAMPLES[stringValue]?.code}</code>
-            </pre>
-          </div>
-      );
-
-    console.log(`stringValue =  ${stringValue}`);
+function App() {  
     console.log('App Component Will Render');
   return (
     <div>
@@ -56,18 +30,9 @@ function App() {
           })}
         </section>
         <section id="examples">
-          <h2>Examples</h2>
-          <menu>
-            <TabButton isSelected={stringValue === 'components'} onSelect={()=>onHandleSelect('components')}>Components</TabButton>
-            <TabButton isSelected={stringValue === 'jsx'} onSelect={()=>onHandleSelect('jsx')}>Jsx</TabButton>
-            <TabButton isSelected={stringValue === 'props'} onSelect={()=>onHandleSelect('props')}>Props</TabButton>
-            <TabButton isSelected={stringValue === 'state'} onSelect={()=>onHandleSelect('state')}>State</TabButton>
-          </menu>
-          {tabContent}
+          <Examples/>
         </section>
       </main>
-      {/* this will not changed automatically , you have to import and use react 'useState'  */}
-      {stringValue}
     </div>
   );
 }
--- React_PlayGround/react-app/src/components/TabButton.jsx
+++
@@ -1,6 +0,0 @@
-// export default function TabButton(props)
-export default function TabButton({children, onSelect, isSelected})
-{
-  console.log('TabButton will render');
-  return <li><button className={isSelected ? 'active' : undefined} onClick={onSelect}>{children}</button></li>
-}
