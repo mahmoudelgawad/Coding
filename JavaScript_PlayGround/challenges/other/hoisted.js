42fe34a330fc1dc40d0d3a53c4b98c95a51e223c
mahmoudelgawad <mahmoud.elgawad@gmail.com>
2026-07-14 12:57:13 +0300

    17 July 2026 - javascript hoisting

--- JavaScript_PlayGround/challenges/other/hoisted.js
+++ JavaScript_PlayGround/challenges/other/hoisted.js
@@ -1,10 +1,22 @@
 test();
+test2();
 
 //OK hoisted
 function test(){
-    //console.log({id: ++nextIndex, age:tasks[0].age});
+    var vartest = 10;
+    const constobjtest = {name:'mahmoud',age:42}
+    //# id=undefined
     console.log({id:indexVar});
-    console.log("just test function");
+    //#reassigned const property value;
+    constobjtest.age = 15; 
+    console.log({isChange:true,constobjtest:constobjtest});
+    console.log("test() - END");
+}
+
+function test2(){
+    var vartest2 = 10;
+    //#reference error - is not defined 
+    //console.log({vartest:vartest});
 }
 
 //REFERENCE ERROR
